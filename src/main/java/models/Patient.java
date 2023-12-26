928f88b2067478d13b9b5cfe84d752890c47b073
lucacupellaro <luca.cupellaro@students.uniroma2.eu>
2023-12-26 22:58:15 +0100

    Ho agggiornato i miei model

--- src/main/java/models/Patient.java
+++ src/main/java/models/Patient.java
@@ -1,4 +1,29 @@
 package models;
 
-public class Patient {
+import models.record.Card;
+import models.record.Credentials;
+import models.record.PersonalInfo;
+
+public class Patient extends User {
+
+    private NutritionalPlan plan;
+    private Nutritionist nutritionist;
+
+    private Card card;
+
+    public Patient(String username, PersonalInfo personalInfo, Credentials credentials){
+        super(username, personalInfo, credentials);
+        card = new Card(
+                "",
+                null
+        );
+    }
+
+    public Nutritionist getNutritionist() {
+        return nutritionist;
+    }
+
+    public void setNutritionist(Nutritionist nutritionist) {
+        this.nutritionist = nutritionist;
+    }
 }
