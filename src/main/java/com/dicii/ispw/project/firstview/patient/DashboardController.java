834d41b5cd5513fcd4f1288367469d788136cef1
lucacupellaro <luca.cupellaro@students.uniroma2.eu>
2024-02-05 12:11:01 +0100

    code smell

--- src/main/java/com/dicii/ispw/project/firstview/patient/DashboardController.java
+++ src/main/java/com/dicii/ispw/project/firstview/patient/DashboardController.java
@@ -3,13 +3,20 @@ import com.dicii.ispw.project.firstview.utils.GUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.io.IOException;
+
 
 public class DashboardController {
 
 
     @FXML
-    public void switchHome(ActionEvent event) throws Exception {
-        GUI.switchPage(event,"/firstGui/patient/dashboard/DashboardHome.fxml");
+    public void switchHome(ActionEvent event) throws IOException {
+        try{
+            GUI.switchPage(event,"/firstGui/patient/dashboard/DashboardHome.fxml");
+        }catch (Exception e){
+            throw e;
+        }
+
     }
 
     public void switchRecipes(ActionEvent event) throws Exception {
