0f48d2a7af4d6f1e85fbb4a52b49f90ac7b676b5
lucacupellaro <luca.cupellaro@students.uniroma2.eu>
2024-02-05 12:17:55 +0100

    code smell

--- src/main/java/com/dicii/ispw/project/firstview/nutritionist/DashboardController.java
+++ src/main/java/com/dicii/ispw/project/firstview/nutritionist/DashboardController.java
@@ -3,24 +3,26 @@ package com.dicii.ispw.project.firstview.nutritionist;
 import com.dicii.ispw.project.firstview.utils.GUI;
 import javafx.event.ActionEvent;
 
+import java.io.IOException;
+
 public class DashboardController {
 
-    public void switchHome(ActionEvent event) throws Exception {
+    public void switchHome(ActionEvent event) throws IOException {
 
         GUI.switchPage(event,"/firstGui/nutritionist/dashboard/DashboardHome.fxml");
 
     }
 
-    public void switchRecipes(ActionEvent event) throws Exception {
+    public void switchRecipes(ActionEvent event) throws IOException {
 
         GUI.switchPage(event,"/firstGui/nutritionist/dashboard/DashboardRecipes.fxml");
 
     }
-    public void switchPlan(ActionEvent event) throws Exception {
+    public void switchPlan(ActionEvent event) throws IOException {
         GUI.switchPage(event,"/firstGui/nutritionist/dashboard/DashboardPlan.fxml");
     }
 
-    public void switchNotificationArea(ActionEvent event) throws Exception {
+    public void switchNotificationArea(ActionEvent event) throws IOException {
         GUI.switchPage(event, "/firstGui/nutritionist/dashboard/NotificationArea.fxml");
     }
 }
