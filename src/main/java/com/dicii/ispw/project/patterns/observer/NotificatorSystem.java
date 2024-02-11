bd8e6bc45af6896e3b6e7b730c379af2f39a834c
P1CC10N4L3XX <piccione.alessandro.02@uniroma2.students.eu>
2024-02-11 22:33:06 +0100

    Bug second gui

--- src/main/java/com/dicii/ispw/project/patterns/observer/NotificatorSystem.java
+++ src/main/java/com/dicii/ispw/project/patterns/observer/NotificatorSystem.java
@@ -7,22 +7,13 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Objects;
 
 public class NotificatorSystem extends UnicastRemoteObject implements Observer{
-    private final SubscribeToNutritionistController subscribeToNutritionistController;
     public NotificatorSystem() throws RemoteException {
-        subscribeToNutritionistController = new SubscribeToNutritionistController();
-    }
-    @Override
-    public void update(Notification notification) throws RemoteException {
-        subscribeToNutritionistController.showNotificationPopUp(notification);
+        super();
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-        NotificatorSystem other = (NotificatorSystem) obj;
-        return Objects.equals(subscribeToNutritionistController, other.subscribeToNutritionistController);
+    public void update(Notification notification) throws RemoteException {
+        SubscribeToNutritionistController subscribeToNutritionistController = new SubscribeToNutritionistController();
+        subscribeToNutritionistController.showNotificationPopUp(notification);
     }
 }
